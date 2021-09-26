04c4fba190892f13d4deff32d76cd60f50c9c959
kllzn <d.s.mudragelya@student.khai.edu>
2021-09-26 21:30:37 +0300

    Chatbot 2 stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -3,3 +3,7 @@ birth_year = '2021'
 
 print('Hello my name is', bot_name)
 print('I was created in', birth_year)
+
+print('Please, remind me your name')
+your_name = input()
+print("What a great name you have, {}!".format(your_name)) # print("What a great name you have," + your_name + "!")
