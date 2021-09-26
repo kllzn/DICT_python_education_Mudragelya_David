d174eb913b611194caf646c09b0b5eb63262064d
kllzn <d.s.mudragelya@student.khai.edu>
2021-09-26 23:21:15 +0300

    Chatbot 5 stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -23,11 +23,31 @@ print('Your age is {}; that\'s a good time to start programming'.format(your_age
 print('Now I will prove to you that I can count to any number you want.')
 
 your_number = int(input())
+i = 0
 for i in range(0, your_number + 1):
     print('{}!'.format(i))
 
 print('Completed, have a nice day!')
 
 
+def test():
+    your_answer = input()
+    if your_answer == '1':
+        print('Completed, have a nice day!')
+    else:
+        print('Please, try again.')
+        test()
+
+
+print('Let\'s test your programming knowledge.')
+print(
+    'Why do we go to school? \n'
+    '1. For fun \n'
+    '2. To  study \n'
+    '3. To communicate with other people \n'
+    '4. To annoy teachers'
+)
+
+test()
 
 print('Congratulations, have a nice day')
