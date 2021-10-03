29694c325623ea967399833d6b4e8c26b5fe694c
kllzn <d.s.mudragelya@student.khai.edu>
2021-10-03 20:25:39 +0300

    2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,3 +2,11 @@ print(
     'HANGMAN \n'
     'The game will be available soon.'
 )
+
+print('HANGMAN')
+answer = 'python'
+player_guess = input('Guess the word:')
+if player_guess == answer:
+    print('You survived!')
+else:
+    print('You lost!')
