395457c048b9ef9b4188d6764c2359c2040d9132
kllzn <d.s.mudragelya@student.khai.edu>
2021-10-06 16:24:34 +0300

    5-th stage complited

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -6,12 +6,36 @@ print(
 )
 
 print('HANGMAN')
+attempts = 8
 options = ['python', 'java', 'javascript', 'php']
 answer = random.choice(options)
-answer1 = answer[:3] + '-' * (len(answer)-3)
+guesses = []
+done = False
 
-player_guess = input('Guess the word {}:'.format(answer1))
-if player_guess == answer:
-    print('You survived!')
-else:
-    print('You lost!')
+while not done:
+
+    for letter in answer:
+        if letter in guesses:
+            print(letter, end='')
+        else:
+            print('_', end='')
+    print('')
+
+    guess = input('Input a letter:')
+    guesses.append(guess)
+    attempts -= 1
+    if guess not in answer:
+        print('That letter doesn\'t appear in the word')
+        if attempts == 0:
+            break
+
+    done = True
+    for letter in answer:
+        if letter not in guesses:
+            done = False
+
+# if done:
+#     print('Congrats')
+# else:
+#     print('Game over')
+print('Thanks for playing!\nWe\'ll see how well you did in the next stage')
