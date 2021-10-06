c952f718fb2340d7ee9778016cd759fc89ab7e22
kllzn <d.s.mudragelya@student.khai.edu>
2021-10-06 18:07:52 +0300

    7-th stage done

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,47 +1,74 @@
 import random
 
-print(
-    'HANGMAN \n'
-    'The game will be available soon.'
-)
-
+# print(
+#     'HANGMAN \n'
+#     'The game will be available soon.'
+# )
 print('HANGMAN')
-attempts = 8
-options = ['python', 'java', 'javascript', 'php']
-answer = random.choice(options)
-guesses = []
-done = False
-# wrong_guesses = []
-while not done:
 
-    for letter in answer:
-        if letter in guesses:
-            print(letter, end='')
+
+def main():
+
+    attempts = 8
+    options = ['python', 'java', 'javascript', 'php']
+    answer = random.choice(options)
+    guesses = []
+    done = False
+    wrong_guesses = []
+    while not done:
+
+        for letter in answer:
+            if letter in guesses:
+                print(letter, end='')
+            else:
+                print('_', end='')
+        print('')
+        guess = input('Input a letter:')
+        if len(guess) != 1:
+            print('You should input a single letter.')
+        elif guess.isupper() is True:
+            print('Please enter a lowercase English letter.')
+        elif guess in wrong_guesses:
+            print('You\'ve already guessed this letter.')
+            # print('Attempts = {}'.format(attempts))
+        elif guess not in answer:
+            attempts -= 1
+            wrong_guesses.append(guess)
+            print('That letter doesn\'t appear in the word')
+            # print('Attempts = {}'.format(attempts))
+        elif guess in guesses:
+            print('You\'ve already guessed this letter.')
+            # print('Attempts = {}'.format(attempts))
+
         else:
-            print('_', end='')
-    print('')
-    guess = input('Input a letter:')
+            guesses.append(guess)
 
-    if guess in guesses:
-        attempts -= 1
-        print('No improvements')
-        # print('Attempts = {}'.format(attempts))
-    if guess in answer:
-        guesses.append(guess)
+        if attempts == 0:
+            break
+
+        done = True
+        for letter in answer:
+            if letter not in guesses:
+                done = False
+
+    if done:
+        print('You guessed the word {}!'.format(answer))
+        print('You survived!')
     else:
-        attempts -= 1
-        # wrong_guesses.append(guess)
-        print('That letter doesn\'t appear in the word')
-        # print('Attempts = {}'.format(attempts))
-    if attempts == 0:
-        break
+        print('You lost!')
 
-    done = True
-    for letter in answer:
-        if letter not in guesses:
-            done = False
 
-if done:
-    print('Congrats')
-else:
-    print('Game over')
+main()
+
+# def __menu__():
+#     menu = input('Type "play" to play the game, "exit" to quit:')
+#     if menu == "play":
+#         main()
+#     elif menu == "exit":
+#         pass
+#     else:
+#         __menu__()
+#
+#
+# __menu__()
+# возможно код отвратительный, но меня не было на лекции и хорошо, что он вообще работает и главное работает как надо
