d77dee35be104738ced3f41aa470175099c3856a
kllzn <d.s.mudragelya@student.khai.edu>
2021-10-06 17:10:24 +0300

    6-th stage done

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -11,7 +11,7 @@ options = ['python', 'java', 'javascript', 'php']
 answer = random.choice(options)
 guesses = []
 done = False
-
+# wrong_guesses = []
 while not done:
 
     for letter in answer:
@@ -20,22 +20,28 @@ while not done:
         else:
             print('_', end='')
     print('')
-
     guess = input('Input a letter:')
-    guesses.append(guess)
-    attempts -= 1
-    if guess not in answer:
+
+    if guess in guesses:
+        attempts -= 1
+        print('No improvements')
+        # print('Attempts = {}'.format(attempts))
+    if guess in answer:
+        guesses.append(guess)
+    else:
+        attempts -= 1
+        # wrong_guesses.append(guess)
         print('That letter doesn\'t appear in the word')
-        if attempts == 0:
-            break
+        # print('Attempts = {}'.format(attempts))
+    if attempts == 0:
+        break
 
     done = True
     for letter in answer:
         if letter not in guesses:
             done = False
 
-# if done:
-#     print('Congrats')
-# else:
-#     print('Game over')
-print('Thanks for playing!\nWe\'ll see how well you did in the next stage')
+if done:
+    print('Congrats')
+else:
+    print('Game over')
