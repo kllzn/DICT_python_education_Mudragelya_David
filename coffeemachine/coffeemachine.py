8bc0320b99399394ce096d60e546d9dfd8193b20
kllzn <d.s.mudragelya@student.khai.edu>
2021-10-07 19:22:53 +0300

    1-st stage complited

---
+++ coffeemachine/coffeemachine.py
@@ -0,0 +1,7 @@
+print('Starting to make a coffee\n'
+      'Grinding coffee beans\n'
+      'Boiling water\n'
+      'Mixing boiled water with crushed coffee beans\n'
+      'Pouring coffee into the cup\n'
+      'Pouring some milk into the cup\n'
+      'Coffee is ready!')
