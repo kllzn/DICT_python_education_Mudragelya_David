3401eae9d0a4ff5a6627c262a391aedaa3214789
kllzn <d.s.mudragelya@student.khai.edu>
2021-10-10 12:42:12 +0300

    2-nd stage complited

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,3 +1,12 @@
+import time
+
+cups = int(input('Write how many cups of coffee you will need:'))
+print('For {} cups of coffee you will need:\n'.format(cups),
+      '{} ml of water\n'.format(cups * 200),
+      '{} ml of milk\n'.format(cups * 50),
+      '{} g of coffee beans'.format((cups * 15)))
+time.sleep(1)
+
 print('Starting to make a coffee\n'
       'Grinding coffee beans\n'
       'Boiling water\n'
