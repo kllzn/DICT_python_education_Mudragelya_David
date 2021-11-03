eb0cbdde51d09e2787a911c960a0309a9da1e60b
kllzn <d.s.mudragelya@student.khai.edu>
2021-11-03 14:08:38 +0200

    6-th stage complited

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,120 +1,120 @@
-from collections import Counter
+class CoffeeMachine:
 
-amounts = Counter({'water_amount': 400, 'milk_amount': 540, 'beans_amount': 120, 'money_amount': 550, 'cups_amount': 9})
+    def __init__(self):
+        # amounts = Counter(
+        #     {'water_amount': 400, 'milk_amount': 540, 'beans_amount': 120, 'money_amount': 550, 'cups_amount': 9})
+        self.water = 400
+        self.milk = 540
+        self.beans = 120
+        self.money = 550
+        self.cups = 9
+
+    def take(self):
+        if self.money == 0:
+            print('There is no money')
+        else:
+            print(f'I gave you {self.money}')
+            self.money = 0
+
+    def fill(self):
+        self.water += int(input('Write how many ml of water you want to add:'))
+        self.milk += int(input('Write how many ml of milk you want to add:'))
+        self.beans += int(input('Write how many grams of coffee beans you want to add:'))
+        self.cups += int(input('Write how many disposable coffee cups you want to add:'))
+
+    def make_espresso(self):
+        # espresso = Counter({'water_amount': 250, 'beans_amount': 16, 'cups_amount': 1, 'money_amount': -4})
+        if self.water < 250:
+            print('Sorry, not enough water!')
+        elif self.beans < 16:
+            print('Sorry, not enough beans!')
+        elif self.cups < 1:
+            print('Sorry, not enough cups!')
+        else:
+            self.water -= 250
+            self.beans -= 16
+            self.cups -= 1
+            self.money += 4
+            print('I have enough resources, making you a coffee!')
+        print('')
+
+    def make_latte(self):
+        # latte = Counter(
+        #     {'water_amount': 350, 'beans_amount': 20, 'cups_amount': 1, 'money_amount': -7, 'milk_amount': 75})
+        if self.water < 350:
+            print('Sorry, not enough water!')
+        elif self.beans < 20:
+            print('Sorry, not enough beans!')
+        elif self.cups < 1:
+            print('Sorry, not enough cups!')
+        elif self.milk < 75:
+            print('Sorry not enough milk!')
+        else:
+            self.water -= 350
+            self.beans -= 20
+            self.cups -= 1
+            self.milk -= 75
+            self.money += 7
+            print('I have enough resources, making you a coffee!')
+        print('')
+
+    def make_cappuccino(self):
+        # cap = Counter(
+        #     {'water_amount': 200, 'beans_amount': 12, 'cups_amount': 1, 'money_amount': -6, 'milk_amount': 100})
+        if self.water < 200:
+            print('Sorry, not enough water!')
+        elif self.beans < 12:
+            print('Sorry, not enough beans!')
+        elif self.cups < 1:
+            print('Sorry, not enough cups!')
+        elif self.milk < 100:
+            print('Sorry not enough milk!')
+        else:
+            self.water -= 200
+            self.beans -= 12
+            self.cups -= 1
+            self.milk -= 100
+            self.money += 6
+            print('I have enough resources, making you a coffee!')
+        print('')
+
+    def remaining(self):
+        print(f'The coffee machine has:\n'
+              f'{self.water} of water\n',
+              f'{self.milk} of milk\n',
+              f'{self.beans} of coffee beans\n',
+              f'{self.cups} of disposable cups\n',
+              f'{self.money} of money')
 
 
-def remaining():
-    print('The coffee machine has:\n'
-          ' {} of water\n'.format(amounts.get('water_amount')),
-          '{} of milk\n'.format(amounts.get('milk_amount')),
-          '{} of coffee beans\n'.format(amounts.get('beans_amount')),
-          '{} of disposable cups\n'.format(amounts.get('cups_amount')),
-          '{} of money'.format(amounts.get('money_amount')))
-    __action__()
-
-
-def take():
-    print('I gave you {}'.format(amounts.get('money_amount')))
-    amounts['money_amount'] = 0
-    print('')
-    __action__()
-
-
-def fill():
-    your_fill = {'water_amount': 0, 'milk_amount': 0, 'beans_amount': 0, 'cups_amount': 0}
-
-    water_amount = int(input('Write how many ml of water you want to add:'))
-    your_fill.update({'water_amount': -water_amount})
-
-    milk_amount = int(input('Write how many ml of milk you want to add:'))
-    your_fill.update({'milk_amount': -milk_amount})
-
-    beans_amount = int(input('Write how many grams of coffee beans you want to add:'))
-    your_fill.update({'beans_amount': -beans_amount})
-
-    cups_amount = int(input('Write how many disposable coffee cups you want to add:'))
-    your_fill.update({'cups_amount': -cups_amount})
-
-    amounts.subtract(your_fill)
-    print('')
-    __action__()
+functions = CoffeeMachine()
 
 
 def buy():
-    choose = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:')
-
+    choose = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:')
     if choose == '1':
-        espresso = Counter({'water_amount': 250, 'beans_amount': 16, 'cups_amount': 1, 'money_amount': -4})
-        if amounts['water_amount'] < espresso['water_amount']:
-            print('Sorry, not enough water!')
-        elif amounts['beans_amount'] < espresso['beans_amount']:
-            print('Sorry, not enough beans!')
-        elif amounts['cups_amount'] < espresso['cups_amount']:
-            print('Sorry, not enough cups!')
-        else:
-            amounts.subtract(espresso)
-            print('I have enough resources, making you a coffee!')
-        print('')
-        __action__()
+        functions.make_espresso()
     elif choose == '2':
-        latte = Counter({'water_amount': 350, 'beans_amount': 20, 'cups_amount': 1, 'money_amount': -7, 'milk_amount': 75})
-        if amounts['water_amount'] < latte['water_amount']:
-            print('Sorry, not enough water!')
-        elif amounts['beans_amount'] < latte['beans_amount']:
-            print('Sorry, not enough beans!')
-        elif amounts['cups_amount'] < latte['cups_amount']:
-            print('Sorry, not enough cups!')
-        elif amounts['milk_amount'] < latte['milk_amount']:
-            print('Sorry, not enough milk!')
-        else:
-            amounts.subtract(latte)
-            print('I have enough resources, making you a coffee!')
-        print('')
-        __action__()
+        functions.make_latte()
     elif choose == '3':
-        cap = Counter({'water_amount': 200, 'beans_amount': 12, 'cups_amount': 1, 'money_amount': -6, 'milk_amount': 100})
-        if amounts['water_amount'] < cap['water_amount']:
-            print('Sorry, not enough water!')
-        elif amounts['beans_amount'] < cap['beans_amount']:
-            print('Sorry, not enough beans!')
-        elif amounts['cups_amount'] < cap['cups_amount']:
-            print('Sorry, not enough cups!')
-        elif amounts['milk_amount'] < cap['milk_amount']:
-            print('Sorry, not enough milk!')
-        else:
-            amounts.subtract(cap)
-            print('I have enough resources, making you a coffee!')
-        print('')
-        __action__()
+        functions.make_cappuccino()
+    elif choose == 'back':
+        actions()
 
 
-def __action__():
-    action = input('Write action (buy, fill, take, remaining, exit):')
-    if action == 'buy':
-        buy()
-    elif action == 'take':
-        take()
-    elif action == 'fill':
-        fill()
-    elif action == 'exit':
-        game_exit()
-    elif action == 'remaining':
-        remaining()
-    else:
-        __action__()
+def actions():
+    while True:
+        action = input('Write action (buy, fill, take, remaining, exit):')
+        if action == 'buy':
+            buy()
+        elif action == 'fill':
+            functions.fill()
+        elif action == 'take':
+            functions.take()
+        elif action == 'remaining':
+            functions.remaining()
+        elif action == 'exit':
+            break
 
 
-def game_exit():
-    pass
-
-
-__action__()
-
-
-# print('Starting to make a coffee\n'
-#       'Grinding coffee beans\n'
-#       'Boiling water\n'
-#       'Mixing boiled water with crushed coffee beans\n'
-#       'Pouring coffee into the cup\n'
-#       'Pouring some milk into the cup\n'
-#       'Coffee is ready!')
+actions()
